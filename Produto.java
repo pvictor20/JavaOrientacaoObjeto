7cbb63335085352c6b6f45ff301b01677c60e584
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-09-26 12:35:56 -0300

    Denominaçao de classes e atributos

--- Produto.java
+++ Produto.java
@@ -1,4 +1,28 @@
 //Classes
+/*
+ * - O nome das classes iniciam-se com letra maiúsculo;
+ * - O nome não deve conter acentuação, caracteres especiais e espaços;
+ * - Nas classes Java não existe implementação da função main();
+ * - Toda classe Java possui a seguinte forma:
+ * - O nome da classe Java DEVE ser o mesmo nome do arquivo Java.
+ * - Tudo que estiver dentro das "chaves" {} faz parteda classe.
+ 
+    public class NomeDaClasse{
+
+    }
+
+ */
+//Atributos
+
+/*
+ * - São características da classe/molde/modelo de dados;
+ * - Pode-se entender atributos como variáveis da classe;
+ * - Outra forma de nomenclatura para os atributos são estados;
+ *  - Atributos são nomeados em letras minúsculas, sem espaço, sem caracteres especials
+ * sem acentuação;
+ */
 public class Produto {
-    
+    String nome;
+    float preco;
+    float desconto;
 }
